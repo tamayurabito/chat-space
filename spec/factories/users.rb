08ee1d09a99ad23f7b895911788a65366e6f5001
tamayurabito <tamayurabito@gmail.com>
2020-08-19 21:31:59 +0900

    set FactoryBot

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(min_length: 8)
+    name              {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {password}
+    password_confirmation {password}
+  end
+
+end
