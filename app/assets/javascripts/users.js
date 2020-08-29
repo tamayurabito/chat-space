ec9c65bfc3f715e3c705dedf00d9bd4ff018835d
tamayurabito <tamayurabito@gmail.com>
2020-08-29 20:52:33 +0900

    add search user Ajax

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,9 @@
+  $('#user-search-field').on("keyup", function(){
+    var input = $("#user-search-field").val();
+    // console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
