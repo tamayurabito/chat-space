ff758c11502824c61046bd3977cd71333e31e34d
tamayurabito <tamayurabito@gmail.com>
2020-08-30 20:49:11 +0900

    add scroll function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -13,6 +13,8 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
         var insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
@@ -20,6 +22,9 @@ $(function(){
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        // console.log('success');
+      }
     })
     .fail(function() {
       alert('error');
