94953121e76537f2133f945c1b1cb25ae66adc7a
tamayurabito <tamayurabito@gmail.com>
2020-08-30 20:42:54 +0900

    get latest messages by Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,21 @@
 $(function(){
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.messages__lists:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+    })
+    .fail(function() {
+    });
   
   function buildHTML(message){
     if (message.image){
