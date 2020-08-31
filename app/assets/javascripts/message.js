3c0d75c34b1bea916b82f827f8dddf1ead5d0fb2
tamayurabito <tamayurabito@gmail.com>
2020-08-31 13:21:21 +0900

    delete console.log

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,7 +23,6 @@ $(function(){
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        // console.log('success');
       }
     })
     .fail(function() {
